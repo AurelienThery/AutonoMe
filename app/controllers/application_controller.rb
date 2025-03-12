895b3dbaad32d1cb199b4035552026795c1c5157
laurent papinaud <laurent.papinaud@gmail.com>
2025-03-12 17:14:15 +0100

    links to navigate through the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :photo_id, :setting_id, :userable_type, :userable_id])
   end
+
+  def after_sign_in_path_for(resource)
+    myprofil_path
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :set_simon, only: [:show]
+  
   def show
     @first_name = @user.first_name
   end
@@ -11,4 +13,11 @@ def set_user
   @user = current_user
   @activities = Activity.where(child_id: @user.userable.id)
   # @educators = Educator.where(id: @activities.educator_id)
+end
+
+def set_simon
+  current_user_id = User.find_by(first_name: "Simon")
+  if current_user.id ==current_user_id
+    @user = current_user
+  end
 end
