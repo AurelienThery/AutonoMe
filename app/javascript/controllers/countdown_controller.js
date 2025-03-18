34f5c7fd16fdaaff9e1b897774c946eaa167bff0
laurent papinaud <laurent.papinaud@gmail.com>
2025-03-18 09:14:21 +0100

    timer controller created

---
+++ app/javascript/controllers/countdown_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="countdown"
+export default class extends Controller {
+
+  static targets = ["display", "progressBar"]
+  static values = {
+    targetTime: Number,
+    duration: Number
+  }
+
+  connect() {
+    this.initialWidth = 60 // largeur initiale en pixels du timer blanc
+    this.interval = setInterval(() => {
+      this.updateCountdown()
+    }, 1000)
+    this.updateCountdown()
+  }
+
+  disconnect() {
+    if (this.interval) {
+      clearInterval(this.interval)
+    }
+  }
+  updateCountdown() {
+    const now = new Date().getTime()
+    const targetTime = this.targetTimeValue
+    const timeRemaining = targetTime - now
+
+    if (timeRemaining <= 0) {
+      // this.displayTarget.textContent = "L'activité est terminée"
+      // Prévoir une action --> pop up pour signaler l
+      this.progressBarTarget.style.width = "60px"
+      clearInterval(this.interval)
+      return
+    }
+
+    // Calcul du temps total
+    const totalDuration = this.durationValue * 1000 // convertir en millisecondes
+
+    // Calcul de la proportion de temps restant
+    const proportion = timeRemaining / totalDuration
+
+    // Calcul de la nouvelle largeur
+    const newWidth = Math.max(0, Math.floor(this.initialWidth * proportion))
+
+    // Application de la nouvelle largeur
+    this.progressBarTarget.style.width = `${newWidth}px`
+
+    // Calcul des secondes restantes (uniquement pour vérification)
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000)
+    const formattedSeconds = seconds.toString()
+
+    // Affichage du résultat
+    this.displayTarget.textContent = `${formattedSeconds}`
+  }
+}
