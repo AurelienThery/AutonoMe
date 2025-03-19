72e4ad9190bef9edc4e0918385ce97b613262a2c
Emilien CLERJUSTE <e.clerjuste18@yahoo.com>
2025-03-19 18:07:07 +0100

    end checklist and checkbox

--- app/javascript/controllers/checklist_controller.js
+++ app/javascript/controllers/checklist_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="checklist"
 export default class extends Controller {
 
-  static targets = ["checkbox", "status"];
+  static targets = ["checkbox", "status", "nbr_notif"];
 
   connect() {
   }
@@ -13,13 +13,14 @@ export default class extends Controller {
 
     if (allChecked) {
       this.statusTarget.innerHTML = '<i class="fa-regular fa-square-check"></i>';
+      this.nbr_notif.connect = "5";
     } else {
       this.statusTarget.innerHTML = '<i class="fa-regular fa-rectangle-xmark"></i>';
     }
   }
 
+
   toggle(event) {
-    console.log(event)
     this.updateStatus(); // Appelé à chaque changement
   }
 }
